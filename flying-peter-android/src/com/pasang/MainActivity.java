b98259c34873fd89a2ac13b136fa7228e4c8ea59
pasang sherpa <pasang_sh@hotmail.com>
2014-03-30 21:45:58 -0700

    no message

--- flying-peter-android/src/com/pasang/MainActivity.java
+++ flying-peter-android/src/com/pasang/MainActivity.java
@@ -6,12 +6,11 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        
-        initialize(new FPGame(), cfg);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		initialize(new FPGame(), cfg);
+	}
 }
